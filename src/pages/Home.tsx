609c6a3a9031e3e647862a350e9fa5f076fd8f81
YasH-c0der <yasssh2004@gmail.com>
2025-04-26 20:35:16 +0530

    error resolving

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -14,8 +14,8 @@ import four from '../assets/stock/4.jpg'
 import five from '../assets/stock/5.avif'
 import six from '../assets/stock/6.avif'
 import seven from '../assets/stock/7.webp'
-import Section_1 from './section_1';
-import Section_2 from './section_2';
+import Section_1 from './Section_1';
+import Section_2 from './Section_2';
 
 
 const Home = () => {
